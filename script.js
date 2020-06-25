741eb71d09efba317234512773353ab914cce700
Michael Olshewski <michael.olshewski@yahoo.com>
2020-06-25 13:56:43 -0400

    Uploaded Current, Broken Code

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     
     // Variables
     const production = false;
+    var currentDay = moment().format("MMMM Do, YYYY");
     var timeOfDay = moment().format("k");
     var mornOrNight = moment().format("A");
     var minuteOfHour = moment().format("m");
@@ -12,37 +13,32 @@ $(document).ready(function() {
     var storage = localStorage.getItem("storedAgendaItems");
 
 
-    /* if (!production) {
-        timeOfDay = 12;
-    } */
-
-    // Onclick listeners
-    $(".saveBtn").on("click", saveToLocalStorage);
+    // Sets the current day to display on the webpage.
+    $("#currentDay").text(currentDay);
 
 
+    // Sets the colors on the divs based on the current hour
     $(".description").each(function() {
         if($(this).attr('data-time') < timeOfDay) {
             $(this).addClass("past");
-        }
+        };
         if($(this).attr('data-time') == timeOfDay) {
             $(this).addClass("present");
-        }
+        };
         if($(this).attr('data-time') > timeOfDay) {
             $(this).addClass("future");
-        }
+        };
+    });
 
-    })
-    
-    function saveToLocalStorage() {
-        console.log($(this).attr('data-id'))
-        console.log($(this).prev().children().text());
-    }
-
+    // If production mode is false, then it console.logs all variables out.
     if (!production) {
+        console.log("Production Mode? " + production)
+        console.log("Todays Date: " + currentDay)
         console.log("Current Time: " + timeOfDay + " " + mornOrNight)
         console.log("Minute of Hour: " + minuteOfHour);
         console.log("Minutes until next hour: " + timeTilNextHour);
-        console.log(localStorage)
+        console.log("Milleseconds: " + milleseconds);
+        console.log(agendaItems)
+        console.log(storage)
     }
-
 });
