aa25c0d4171c3e59de3d70a6b4f76212fae577b8
Michael Olshewski <michael.olshewski@yahoo.com>
2020-06-27 21:11:36 -0400

    Uploaded Current/Working Code

--- script.js
+++ script.js
@@ -1,47 +1,158 @@
 // Load Script once page has loaded and is ready
 $(document).ready(function() {
+
+    var workDay = [
+        {
+            id: "0",
+            hour: "9",
+            militaryTime: "9",
+            amPM: "am",
+            agendaItem: ""
+        },
+        {
+            id: "1",
+            hour: "10",
+            militaryTime: "10",
+            amPM: "am",
+            agendaItem: ""
+        },
+        {
+            id: "2",
+            hour: "11",
+            militaryTime: "11",
+            amPM: "am",
+            agendaItem: ""
+        },
+        {
+            id: "3",
+            hour: "12",
+            militaryTime: "12",
+            amPM: "pm",
+            agendaItem: ""
+        },
+        {
+            id: "4",
+            hour: "1",
+            militaryTime: "13",
+            amPM: "pm",
+            agendaItem: ""
+        },
+        {
+            id: "5",
+            hour: "2",
+            militaryTime: "14",
+            amPM: "pm",
+            agendaItem: ""
+        },
+        {
+            id: "6",
+            hour: "3",
+            militaryTime: "15",
+            amPM: "pm",
+            agendaItem: ""
+        },
+        {
+            id: "7",
+            hour: "4",
+            militaryTime: "16",
+            amPM: "pm",
+            agendaItem: ""
+        },
+        {
+            id: "8",
+            hour: "5",
+            militaryTime: "17",
+            amPM: "pm",
+            agendaItem: ""
+        }
+    ]
     
     // Variables
-    const debugMode = true;
+    const debugMode = false;
     var currentDay = moment().format("MMMM Do, YYYY");
     var timeOfDay = moment().format("k");
     var mornOrNight = moment().format("A");
     var minuteOfHour = moment().format("m");
     var timeTilNextHour = 60 - minuteOfHour;
     var milleseconds = timeTilNextHour * 60 * 1000;
-    var agendaItems = [];
-    var storage = localStorage.getItem("storedAgendaItems");
-
-
 
     // Sets the current day to display on the webpage.
     $("#currentDay").text(currentDay);
 
+    function init() { // Initialize Stored Data
+        var storedDay = JSON.parse(localStorage.getItem("workDay"));
+        if (storedDay) {
+            workDay = storedDay;
+        }
+        displayAgendaItems();
+    };
+    
+    function displayAgendaItems() { // displayReminders
+        workDay.forEach(function(_thisHour) {
+            $(`#${_thisHour.id}`).text(_thisHour.agendaItem);
+            $("textarea[data-id='" + _thisHour.id + "']").val(_thisHour.agendaItem);
+        })
+    };
 
-    // Sets the colors on the divs based on the current hour
-    $(".description").each(function() {
-        if(( parseInt($(this).attr('data-time')) ) < timeOfDay) {
-            $(this).addClass("past");
-        };
-        if(( parseInt($(this).attr('data-time')) ) == timeOfDay) {
-            $(this).addClass("present");
-        };
-        if(( parseInt($(this).attr('data-time')) ) > timeOfDay) {
-            $(this).addClass("future");
-        };
-    });
+    function saveAgendaItems() { //saveReminders
+        localStorage.setItem("workDay", JSON.stringify(workDay));
+        console.log(workDay);
+    }
 
-    $(document).on("click", "i", function(event) {
-        event.preventDefault();
+    workDay.forEach(function(thisHour) {
 
-        if (debugMode) { console.log($(this))};
+        // Creates Table
+        var timeTable = $("<table>");
+        $(".container").append(timeTable);
 
-        console.log($(this).prev("td").children().text(JSON.parse));
+        // Creates Time Row
+        var timeRow = $("<tr>");
+        timeTable.append(timeRow);
 
-        localStorage.setItem("storedAgendaItems", JSON.stringify(agendaItems));
+        // Creates Agenda Rows
+        var timeBlock = $("<td>").addClass("time-block hour");
+        var descBlock = $("<td>").addClass("description").attr("data-time", thisHour.hour);
+        var saveBlock = $("<td>").addClass("saveBtnContainer");
+
+        var timeTag = thisHour.hour + " " + thisHour.amPM;
+        timeBlock.append(timeTag);
+
+        var agendaData = $("<textarea>").attr("data-id", thisHour.id);
+        descBlock.append(agendaData);
+        
+        // Creates Save Button
+        var saveButton = $("<i class='far fa-save fa-lg'>");
+        var saveAgenda = $("<button>").attr("class", "saveBtn").attr("data-id", thisHour.id);
+        saveAgenda.append(saveButton);
+        saveBlock.append(saveAgenda);
+        timeRow.append(timeBlock, descBlock, saveBlock);
+
+        function updateHourColors() {
+            if (parseInt(thisHour.militaryTime) < timeOfDay) {
+                descBlock.addClass("past");
+            }
+            if (parseInt(thisHour.militaryTime) == timeOfDay) {
+                descBlock.addClass("present");
+            }
+            if (parseInt(thisHour.militaryTime) > timeOfDay) {
+                descBlock.addClass("future");
+            }
+
+            setTimeout(updateHourColors, 60 * 60 * 1000);
+        }
+
+        setTimeout(updateHourColors, timeTilNextHour);
     })
 
-    // If production mode is false, then it console.logs all variables out.
+    $(document).on("click", ".saveBtn", function(event) {
+        workDay[$(this).attr("data-id")].agendaItem = $(this).parent().siblings('.description').children("textarea").val();
+        saveAgendaItems();
+    });
+
+    // Initializes Page
+    init();
+
+    // If debug mode is true, then it console.logs all variables out.
     if (debugMode) {
         console.log("Debug Mode Enabled? " + debugMode)
         console.log("Todays Date: " + currentDay)
@@ -49,7 +160,7 @@ $(document).ready(function() {
         console.log("Minute of Hour: " + minuteOfHour);
         console.log("Minutes until next hour: " + timeTilNextHour);
         console.log("Milleseconds: " + milleseconds);
-        console.log(agendaItems)
-        console.log(storage)
+        console.log(workDay)
+        console.log(localStorage)
     }
 });
